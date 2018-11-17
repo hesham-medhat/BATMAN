5e124c350f85593b49811e9cc9a22e8f9923f5c9
Hesham Medhat <heshammedhat5@gmail.com>
2018-11-17 14:22:20 +0200

    Comment BatMonitor.h and add waiting counter

--- BatMonitor.h
+++ BatMonitor.h
@@ -7,22 +7,30 @@
 
 class BatMonitor {
 private:
-    BatMonitor* instance;
+    static BatMonitor* instance;
+
+    /* Number of BATs currently waiting */
+    int waiting;
 
     pthread_mutex_t mutex;
 
+    /* Condition variables for passing BATs from initial directions */
     pthread_cond_t nPass;
     pthread_cond_t ePass;
     pthread_cond_t wPass;
     pthread_cond_t sPass;
 
+    /* Private constructor only accessible by getInstance applying singleton design pattern */
     BatMonitor();
 
+    /* Deadlock checker and resolver. Allows north to pass resolving deadlock if present */
     void check();
 
 public:
-    static BatMonitor getInstance();
+    /* Ensures there is only one instance of this type and returns it */
+    static BatMonitor* getInstance();
 
+    /* Monitor operations called by BATs */
     void arrive(BAT &bat);
     void cross(BAT &bat);
     void leave(BAT &bat);
