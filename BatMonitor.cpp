9306b89d52651654b3180479658b1a8cda69d120
Hesham Medhat <heshammedhat5@gmail.com>
2018-11-17 14:21:24 +0200

    BatMonitor skeleton and creation

--- BatMonitor.cpp
+++ BatMonitor.cpp
@@ -1,1 +1,55 @@
+#include <pthread.h>
 #include "BatMonitor.h"
+
+BatMonitor::BatMonitor() {
+    pthread_mutex_init(&mutex, nullptr);
+
+    pthread_cond_init(&nPass, nullptr);
+    pthread_cond_init(&ePass, nullptr);
+    pthread_cond_init(&wPass, nullptr);
+    pthread_cond_init(&sPass, nullptr);
+
+    pthread_cond_signal(&nPass);
+    pthread_cond_signal(&ePass);
+    pthread_cond_signal(&wPass);
+    pthread_cond_signal(&sPass);
+
+    waiting = 0;
+}
+
+void BatMonitor::check() {
+    if (waiting == 4) { // All are waiting
+        pthread_cond_signal(&wPass); // As if west has just passed to let North pass resolving deadlock
+    }
+}
+
+BatMonitor* BatMonitor::getInstance() {
+    if (instance == nullptr) {
+        instance = new BatMonitor();
+    }
+    return instance;
+}
+
+void BatMonitor::arrive(BAT &bat) {
+    pthread_mutex_lock(&mutex);
+    // TODO: Implement arrival
+
+    check();
+    pthread_mutex_unlock(&mutex);
+}
+
+void BatMonitor::cross(BAT &bat) {
+    pthread_mutex_lock(&mutex);
+    // TODO: Implement cross
+
+    check();
+    pthread_mutex_unlock(&mutex);
+}
+
+void BatMonitor::leave(BAT &bat) {
+    pthread_mutex_lock(&mutex);
+    // TODO: Implement leave
+
+    check();
+    pthread_mutex_unlock(&mutex);
+}
